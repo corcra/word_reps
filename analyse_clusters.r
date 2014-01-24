e1b39bf0cddecfb3da556f34441111a0c813d84a
Stephanie Hyland <steph.hyland@gmail.com>
2014-01-24 16:10:48 -0500

    automatically plots histograms

--- analyse_clusters.r
+++ analyse_clusters.r
@@ -36,3 +36,11 @@ for (c in top_10){
     cat("Cluster: ",c,", mean distance: ",clust_means[c],",sd: ",clust_sd[c],"\n",sep="")
     cwords(da,c)
 }
+
+# Now, let's plot some histograms.
+cat("Plotting histograms!\n")
+pdf(paste("hists_",n_clust,".pdf",sep=""))
+par(mfrow=c(2,1))
+hist(da$"dist",breaks=50,col="palegreen3",main=paste("Histogram of distance to centroid (n_clust =",n_clust,")"),xlab="Point-centroid distance")
+hist(clust_means,breaks=50,col="deepskyblue2",main=paste("Histogram of mean point-centroid distance in cluster (n_clust =",n_clust,")"),xlab="Mean point-centroid distance in cluster")
+dev.off()
